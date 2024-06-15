8466e1fb8865931c1f197973050cc4a1646cb0a2
Sana-0511 <svnkanbargi@gmail.com>
2024-06-16 05:25:32 +0530

    Kmain Navbar Added

--- margshala/src/components/KMainPg/KMainPg.jsx
+++ margshala/src/components/KMainPg/KMainPg.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { db, auth } from '../../firebase';
+import { Link } from 'react-router-dom'; 
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import './KMainPg.css';
+
 
 function KMainPg() {
   const [jobs, setJobs] = useState([]);
@@ -46,6 +48,39 @@ function KMainPg() {
   }, [userInterests]);
 
   return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          <img src="https://margshala.com/wp-content/uploads/2023/08/Margshala-Logo.png" width="150" height="60" alt="Margshala Logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/programs">Programs</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/mentor">Be A Mentor</Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact" rel="noopener noreferrer">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     <div className="main-container">
       <h2>Courses</h2>
       <div className="card-container">
@@ -66,12 +101,13 @@ function KMainPg() {
         {jobs.map((job, index) => (
           <div key={index} className="card">
             <h3>{job.jobName}</h3>
-            <p>{job.jobDesc}</p>
+            <p>{job.jobDesc.length > 100 ? job.jobDesc.substring(0, 100) + '...' : job.jobDesc}</p>
             <a href={job.links} target="_blank" rel="noopener noreferrer">Learn More</a>
           </div>
         ))}
       </div>
     </div>
+  </>
   );
 }
 
