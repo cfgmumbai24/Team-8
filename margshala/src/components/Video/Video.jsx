c30f4203af5fcd6fe918a0983dd91145eaabbe1e
avbhutani <avbhutani3@gmail.com>
2024-06-16 01:44:25 +0530

    Fixed the video on main page

--- margshala/src/components/Video/Video.jsx
+++ margshala/src/components/Video/Video.jsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import './Video.css'; // Import CSS for styling the video component
-
-const Video = () => {
-  return (
-    <div className="container my-2">
-      <div className="row justify-content-center">
-        <div className="col-md-12">
-          <div className="embed-responsive embed-responsive-16by9">
-            <video className="embed-responsive-item" controls autoPlay muted loop>
-              <source src="C:\Users\Ishita\OneDrive\Desktop\gg\Team-8\margshala\src\components\assets\big_buck_bunny_720p_1mb.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        </div>
-      </div>
+import React from 'react'
+import video from '../assets/video.mp4'
+import ReactPlayer from 'react-player'
+function MyPlayer() {
+  return ( 
+    <div>
+    <ReactPlayer controls url = {video} height = "100" width="750" />
     </div>
-  );
-};
+  )
+}
 
-export default Video;
+
+export default MyPlayer
